0efbf1c2892f0d795cac816027b67d6b4babcf05
Lev Leyman <lev.leiman@yandex.ru>
2023-10-17 22:46:10 +0400

    add vite service

--- src/main/java/home/server/jwebplayer/service/vite/ViteService.java
+++ src/main/java/home/server/jwebplayer/service/vite/ViteService.java
@@ -17,9 +17,21 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Сервис интеграции с vite.
+ * Реализует получение ссылки на файл с учетом конфигурации:
+ * - хост dev сервера vite
+ * - путь из манифеста production сборки
+ */
 @Component
 public class ViteService
 {
+    /**
+     * Настройки vite (путь к манифесту и хост)
+     * Хост приоритетнее манифеста,
+     * если указан хост - ссылка на файл будет с этим хостом,
+     * иначе будет использоваться относительные ссылки из манифеста
+     */
     private final ViteProperties properties;
 
     private Map<String, FileEntry> manifest;
@@ -27,21 +39,18 @@ public class ViteService
     @Autowired
     public ViteService(ViteProperties properties)
     {
+        if (properties.host() == null && properties.manifestPath() == null) {
+            throw new IllegalArgumentException("Настройки vite некорректны, требуется host или manifest-path");
+        }
+
         this.properties = properties;
     }
 
     public String createScriptUrl(String script)
     {
-        if (manifest != null) {
-            var entry = manifest.get(script);
+        var url = getUrlByHost(script);
 
-            if (entry != null) {
-                return "player/" + entry.file;
-            }
-
-        }
-
-        return properties.host() != null ? properties.host() + "/" + script : script;
+        return url != null ? url : getUrlByManifest(script);
     }
 
     @PostConstruct
@@ -65,13 +74,39 @@ public class ViteService
         });
     }
 
+    /**
+     * Получение ссылки на файл через манифест
+     * @param script Относительный путь к файлу
+     * @return Ссылка на файл
+     */
+    private String getUrlByManifest(String script)
+    {
+        if (manifest != null) {
+            var entry = manifest.get(script);
+
+            if (entry != null) {
+                return "player/" + entry.file;
+            }
+
+        }
+
+        return null;
+    }
+
+    /**
+     * Получение ссылки на файл с хостом vite
+     * @param script Относительный путь к файлу
+     * @return Ссылка на файл
+     */
+    private String getUrlByHost(String script)
+    {
+        return properties.host() != null ? properties.host() + "/" + script : null;
+    }
+
     private static class FileEntry
     {
         public List<String> css;
         public String file;
-        public Boolean isEntry;
-        public String src;
-        public List<String> imports;
     }
 
 }
